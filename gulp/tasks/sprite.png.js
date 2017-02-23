f22a08088c1a84e9a4f57dc8f10756d894555b5c
beLemoth <ilin.job@gmail.com>
2017-02-23 12:58:40 +0300

    upload pug base

---
+++ gulp/tasks/sprite.png.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('sprite:png', function () {
+    var spriteData = $.gulp.src('./source/images/icons/*.png')
+      .pipe($.spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+      }));
+    var imgStream = spriteData.img
+      .pipe($.gulp.dest($.config.root + '/assets/img'));
+    var cssStream = spriteData.css
+      .pipe($.gulp.dest($.config.root + '/assets/css'));
+    return $.merge(imgStream, cssStream);  
+  });
+};
+
+  
