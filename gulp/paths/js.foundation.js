78537f4a89bfb54dfee7830e204dbe1b50bfee85
beLemoth <ilin.job@gmail.com>
2017-02-24 15:18:06 +0300

    add all styles

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = [
-  './bower_components/jquery/dist/jquery.min.js'
+  './bower_components/jquery/dist/jquery.min.js',
+  './bower_components/photobox/photobox/jquery.photobox.js',
+  './bower_components/page-scroll-to-id/jquery.malihu.PageScroll2id.js'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,13 @@
 (function() {
   'use strict';
 
+  // photo gallery
+  $('#gallery').photobox('a',{ time:0 });
+
+  // product scroll
+  $("a.scroll2table").mPageScroll2id({
+  	
+  });
+
   console.log('ready');
 })();
